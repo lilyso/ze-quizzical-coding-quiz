7db2bfae54d259d99e5de5d10ef2447fd0a294b9
lilys0 <lsouryaphanh@gmail.com>
2021-09-14 00:18:43 +1000

    highscore leaderboard

--- assets/js/highscores-script.js
+++ assets/js/highscores-script.js
@@ -1,53 +1,34 @@
 var leaderBoard = document.getElementById("score-table");
 var clearScore = document.getElementById("clear-scores");
 var liScore = document.createElement("li");
-var gameScores = [];
 var storedGames = JSON.parse(localStorage.getItem("games"));
 
 // The following function renders items in a list as <li> elements
 function renderScores() {
-  leaderBoard.textContent = gameScores.length;
-  for (var i = 0; i < gameScores.length; i++) {
-    var gameScore = gameScores[i];
+  for (var i = 0; i < storedGames.length; i++) {
+    liScore.setAttribute(
+      "style",
+      "background-color:#c7f9cc; padding:20px; margin:20px"
+    );
+
+    leaderBoard.appendChild(liScore);
+
+    liScore.textContent =
+      storedGames[i]["initials"] + " - " + storedGames[i]["score"];
   }
-  var li = document.createElement("li");
-  li.textContent = gameScore;
-  li.setAttribute("data-index", i);
-  leaderBoard.append(li);
-  liScore.textContent = "test";
+
+  //     leaderBoard.appendChild(liScore);
+  //   }
 }
 
-function init() {
-  if (storedGames !== null) {
-    gameScores = storedGames;
-  }
-}
-
-renderScores();
-init();
-
-// Render scores
-// function highscorePage() {
-//   var ol = document.createElement("ol");
-//   leaderBoard.append(ol);
-//   leaderBoard.append(liScore);
-//   liScore.setAttribute(
-//     "style",
-//     "background-color:#c7f9cc; padding:20px; margin:20px"
-//   );
-//   var games = localStorage.getItem("games");
-//   liScore.textContent = games[index];
-//   for (let index = 0; index < games.length; index++) {
-//     leaderBoard.append(ol);
-//     leaderBoard.append(liScore);
-//     liScore.setAttribute(
-//       "style",
-//       "background-color:#c7f9cc; padding:20px; margin:20px"
-//     );
+// function init() {
+//   if (storedGames !== null) {
+//     gameScores = storedGames;
 //   }
 // }
 
-// highscorePage();
+renderScores();
+// init();
 
 // clearScore.addEventListener("click", function () {
 //   liScore.textContent = "";
